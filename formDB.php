44b81196abab39ab16e80cf9013af57d007c9ef3
B00ZI <iraamaneyahya@gmail.com>
2025-11-23 19:47:40 +0100

    switching mysqli to

--- formDB.php
+++ formDB.php
@@ -1,10 +1,19 @@
 <?php
 $serverName ="localhost";
 $username ="root";
-$password ="";
+$password ="s";
 $dbName ="form_data" ;
+// DSN string: driver (mysql), host, dbname, and character set
 
-$conn = new mysqli($serverName , $username , $password , $dbName );
+$dsn = "mysql: host:$serverName; dbName=$dbName " ;
+try {
+    $pdo = new PDO($dsn , $username , $password) ;
+    echo "status ok" ;
+}catch(PDOException $e) {
+   echo "error " ; 
+}
+
+// $conn = new mysqli($serverName , $username , $password , $dbName );
 
 
 
