ca63ead56fd6041aea6d714fb0784b771fe29d7a
B00ZI <iraamaneyahya@gmail.com>
2025-12-10 21:50:16 +0100

    refactor: improve code formatting and structure for database connection and message display

--- formDB.php
+++ formDB.php
@@ -1,26 +1,40 @@
 <?php
-$serverName ="localhost";
-$username ="root";
-$password =""; 
-$dbName ="form_data" ;
+$serverName = "localhost";
+$username = "root";
+$password = "";
+$dbName = "form_data";
 // DSN string: driver (mysql), host, dbname, and character set
 
-$dsn = "mysql:host=$serverName;dbname=$dbName" ;
-try {
-    $pdo = new PDO($dsn , $username , $password) ;
-    $pdo ->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
-    $pdo ->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE , PDO::FETCH_ASSOC);
-    $stmt = $pdo -> query("SELECT * FROM contacts");
-    $all_contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    echo "status ok" ;
-    print_r($all_contacts) ;
-}catch(PDOException $e) {
-   echo "error " ; 
-}
+$dsn = "mysql:host=$serverName;dbname=$dbName";
 
-// $conn = new mysqli($serverName , $username , $password , $dbName );
+$pdo = new PDO($dsn, $username, $password);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
+$stmt = $pdo->query("SELECT * FROM contacts");
+$messages = $stmt->fetchAll();
 
-
-
+print_r($messages);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>messages</title>
+</head>
+
+<body>
+    <h1 style="text-align: center;">All messages</h1>
+    <?php foreach ($messages as $message): ?>
+        <div style="background-color: #333; color:aliceblue; padding:10px; width:50vw; margin:10px auto; border-radius:10px; " >
+            <h2><?= $message["name"] ?></h2>
+            <h4><?= $message["email"] ?></h4>
+            <p><?= $message["message"] ?></p>
+        </div>
+
+    <?php endforeach ?>
+</body>
+
+</html>
